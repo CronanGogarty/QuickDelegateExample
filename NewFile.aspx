a99d6a74c01bf0aae7191474d4f3a307a439c4db
Cronán Gogarty <cronangogarty@outlook.com>
2016-04-30 14:46:57 -0700

    Another commit

--- NewFile.aspx
+++ NewFile.aspx
@@ -3,5 +3,10 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Button runat="server" ID="cmdClickDelegate" Text="Click Me"/>
+    <br />
+    <asp:Button runat="server" ID="cmdClickAnonymousDelegate" Text="Anonymous Delegate" />
+    <br />
+    <asp:Label runat="server" ID="lblMessage"></asp:Label>
 </asp:Content>
 
--- NewFile.aspx.cs
+++ NewFile.aspx.cs
@@ -7,11 +7,35 @@ using System.Web.UI.WebControls;
 
 public partial class NewFile : System.Web.UI.Page
 {
+   
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        //choose the event handler that will handle the .Click event when fired by cmdClickDelegate
+        cmdClickDelegate.Click += cmdClickDelegate_Click;
+
+        //the .Click event subscribes to the anonymous delegate which handles the event inline
+        cmdClickAnonymousDelegate.Click += delegate(object s, EventArgs arrrggg)
+        {
+            lblMessage.Text = "Anonymous delegate example";
+        };
+
         if (!Page.IsPostBack)
         {
             //do some stuff
+            List<string> animals = new List<string>();
+            animals.Add("Dog");
+            animals.Add("Cat");
+            animals.Add("Racoon");
         }
     }
+    protected void cmdClickDelegate_Click(object sender, EventArgs e)
+    {
+        lblMessage.Text = "Hellow Worddld - Haouuuww aaaaarrr ya?";
+    }
+
+    //protected void cmdClickAnonymousDelegate(object sender, EventArgs e)
+    //{
+    //    lblMessage.Text = "Anonymous delegate example";
+    //}
 }
